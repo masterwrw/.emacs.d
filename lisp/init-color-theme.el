13e1d799f0171db4466c979713b12bcf7282a72c
masterwrw <master.wrw@gmail.com>
2018-01-06 00:29:13 +0800

    add leuven theme

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -4,7 +4,18 @@
 (color-theme-initialize)
 ;(color-theme-deep-blue)
 
+
+;; dracula-theme, dark theme
 (require-package 'dracula-theme)
-(load-theme 'dracula t)
+;(load-theme 'dracula t)
+
+
+;; leuven-theme, light and dark theme
+(require-package 'leuven-theme)
+;(load-theme 'leuven t) ; light
+(require 'leuven-dark-theme)
+(load-theme 'leuven-dark t)
+
+
 
 (provide 'init-color-theme)
