6e45e071b501755ab83c5c8069ea7ed68e4b416b
masterwrw <master.wrw@gmail.com>
2018-01-07 15:57:52 +0800

    change default theme

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -6,15 +6,15 @@
 
 
 ;; dracula-theme, dark theme
-(require-package 'dracula-theme)
+;(require-package 'dracula-theme)
 ;(load-theme 'dracula t)
 
 
 ;; leuven-theme, light and dark theme
 (require-package 'leuven-theme)
-;(load-theme 'leuven t) ; light
-(require 'leuven-dark-theme)
-(load-theme 'leuven-dark t)
+(load-theme 'leuven t) ; light
+;(require 'leuven-dark-theme)
+;(load-theme 'leuven-dark t)
 
 
 
