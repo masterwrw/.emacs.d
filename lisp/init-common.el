56933c1f4d18c5a72dd2f07f9bad6a09e2ac27d8
masterwrw <master.wrw@gmail.com>
2018-01-10 21:18:24 +0800

    redefined keyboard-escape-quit function

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -15,4 +15,17 @@
   (interactive)
   (load-file (expand-file-name "init.el" user-emacs-directory)))
 
+
+;; Redefined keyboard-escape-quit function, see https://stackoverflow.com/questions/557282/in-emacs-whats-the-best-way-for-keyboard-escape-quit-not-destroy-other-windows#
+(defadvice keyboard-escape-quit (around my-keyboard-escape-quit activate)
+  (let (orig-one-window-p)
+    (fset 'orig-one-window-p (symbol-function 'one-window-p))
+    (fset 'one-window-p (lambda (&optional nomini all-frames) t))
+    (unwind-protect
+        ad-do-it
+      (fset 'one-window-p (symbol-function 'orig-one-window-p)))))
+
+
+
+
 (provide 'init-common)
