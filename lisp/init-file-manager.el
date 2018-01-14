8d93ab1d68255f186b8ec500963ef631de1373f4
masterwrw <master.wrw@gmail.com>
2018-01-14 18:46:01 +0800

    add dired-x config

--- lisp/init-file-manager.el
+++ lisp/init-file-manager.el
@@ -4,4 +4,27 @@
 (require-package 'treemacs)
 (require-package 'findr)
 
+
+;; direc-x configuration, for use virtual dired.
+(load "dired-x")
+
+;(add-hook 'dired-load-hook
+;          (lambda ()
+;            (load "dired-x")
+;            ;; Set dired-x global variables here.  For example:
+;            ;; (setq dired-guess-shell-gnutar "gtar")
+;            ;; (setq dired-x-hands-off-my-keys nil)
+;            ))
+(add-hook 'dired-mode-hook
+          (lambda ()
+            ;; Set dired-x buffer-local variables here.  For example:
+            (dired-omit-mode 1)
+            ))
+
+(setq auto-mode-alist (cons '("[^/]\\.dired$" . dired-virtual-mode)
+                                   auto-mode-alist))
+
+
+
+
 (provide 'init-file-manager)
