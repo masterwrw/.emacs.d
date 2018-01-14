63135586461becd0eb80c86d8cdc872fe11da2c1
masterwrw <master.wrw@gmail.com>
2018-01-15 01:07:21 +0800

    add package nyan-mode, a cat on mode line

--- lisp/init-color-theme.el
+++ lisp/init-color-theme.el
@@ -40,7 +40,30 @@
 
 
 
-
+(require-package 'nyan-mode)
+(require 'nyan-mode)
+(setq default-mode-line-format
+      (list ""
+            'mode-line-modified
+            "<"
+            "kirchhoff"
+            "> "
+            "%10b"
+            '(:eval (when nyan-mode (list (nyan-create))));;注意添加此句到你的format配置列表中
+            " "
+            'default-directory
+            " "
+            "%[("
+            'mode-name
+            'minor-mode-list
+            "%n"
+            'mode-line-process
+            ")%]--"
+            "Line %l--"
+            '(-3 . "%P")
+            "-%-"))
+(nyan-mode t)
+(nyan-start-animation)
 
 
 
