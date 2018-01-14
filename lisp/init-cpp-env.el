f1e598968d76a9129ac5dbb839bca0c0dc290e33
masterwrw <master.wrw@gmail.com>
2018-01-15 00:04:28 +0800

    add package irony, irony-company. change cpp env config

--- lisp/init-cpp-env.el
+++ lisp/init-cpp-env.el
@@ -1,15 +1,6 @@
 ;;; C++ programming environment
 
-;; build in cc-mode config
-(setq c-basic-offset 4)
-
-
-;; function-args
-(require-package 'function-args)
-(fa-config-default)
-
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
 
 ;; qt-pro-mode
 ;(require-package 'qt-pro-mode)
@@ -19,18 +10,54 @@
   :mode ("\\.pro\\'" "\\.pri\\'"))
 
 
-;; ggtags
-;(require-package 'ggtags)
-;(add-hook 'c-mode-common-hook
-;          (lambda ()
-;            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-;              (ggtags-mode 1))))
-;
-;;; counsel-gtags
-;(require-package 'counsel-gtags)
+;;======================== Auto complete settings ====================
+;; function-args
+(require-package 'function-args)
+(fa-config-default)
 
 
-;; Not use gtags.
+;; dumb-jump
+(require-package 'dumb-jump)
+(require 'dumb-jump)
+(dumb-jump-mode)
+
+
+;;-------------------------irony for auto complete--------------------
+;; irony
+(require-package 'irony)
+(require 'irony)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(diminish 'irony-mode "iro")
+
+(when *is-windows*
+  (progn
+    ;; Windows performance tweaks
+    (when (boundp 'w32-pipe-read-delay)
+      (setq w32-pipe-read-delay 0))
+    ;; Set the buffer size to 64K on Windows (from the original 4K)
+    (when (boundp 'w32-pipe-buffer-size)
+      (setq irony-server-w32-pipe-buffer-size (* 64 1024)))))
+
+
+;; company-irony
+(require-package 'company-irony)
+(require 'company-irony)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+
+;; company-irony-c-headers
+(require-package 'company-irony-c-headers)
+(require 'company-irony-c-headers)
+;; Load with `irony-mode` as a grouped backend
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony-c-headers))
+
+
+;;======================== fast jump settings =====================
+;;----------------------------ctags---------------------------------
 ;; counsel-etags, Fast and complete Ctags/Etags solution using ivy.
 (require-package 'counsel-etags)
 (require 'counsel-etags)
@@ -58,54 +85,26 @@
     (add-hook 'after-save-hook
               'counsel-etags-virtual-update-tags 'append 'local)))
 
-;; You can change callback counsel-etags-update-tags-backend to update tags file using your own solution,
+; You can change callback counsel-etags-update-tags-backend to update tags file using your own solution,
 ;(setq counsel-etags-update-tags-backend (lambda () (shell-command "find . -type f -iname \"*.[ch]\" | etags -")))
 
 
+;;-------------------------------cedet-----------------------------
+;(require 'cedet)
+;(require 'cc-mode)
+;(require 'semantic)
+;(global-semanticdb-minor-mode 1)
+;(global-semantic-idle-scheduler-mode 1)
+;(global-semantic-stickyfunc-mode 1)
+;(semantic-mode 1)
+;
+;(require 'ede)
+;(global-ede-mode)
+;
+;;; Include setting
+;(require 'semantic/bovine/gcc)
+;(require 'semantic/bovine/c)
 
-
-(setq company-backends '((company-c-headers company-dabbrev-code company-gtags)))
-
-
-;; dumb-jump
-(require-package 'dumb-jump)
-(require 'dumb-jump)
-(dumb-jump-mode)
-
-
-;; cedet
-(require 'cc-mode)
-(require 'semantic)
-
-(global-semanticdb-minor-mode 1)
-(global-semantic-idle-scheduler-mode 1)
-(global-semantic-stickyfunc-mode 1)
-
-(semantic-mode 1)
-
-
-;; Enable EDE only in C/C++
-(require 'ede)
-(global-ede-mode)
-
-
-; ;; System header path
-; (require 'semantic/bovine/gcc nil 'noerror)
-; 
-; (setq qt5-core-dir "d:/Qt/Qt5.8.0/5.8/mingw53_32/include/QtCore")
-; (setq qt5-gui-dir "d:/Qt/Qt5.8.0/5.8/mingw53_32/include/QtGui")
-; (setq qt5-network-dir "d:/Qt/Qt5.8.0/5.8/mingw53_32/include/QtNetwork")
-; (semantic-add-system-include qt5-core-dir 'c++-mode)
-; (semantic-add-system-include qt5-gui-dir 'c++-mode)
-; (semantic-add-system-include qt5-network-dir 'c++-mode)
-; (add-to-list 'auto-mode-alist (cons qt5-core-dir 'c++-mode))
-; (add-to-list 'auto-mode-alist (cons qt5-gui-dir 'c++-mode))
-; (add-to-list 'auto-mode-alist (cons qt5-network-dir 'c++-mode))
-; ;; fix void variable error, https://lists.gnu.org/archive/html/emacs-devel/2011-12/msg00683.html
-; (defvar semantic-lex-c-preprocessor-symbol-file '())
-; (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt5-core-dir "/qconfig.h"))
-; (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt5-core-dir "/qglobal.h"))
-; (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt5-core-dir "/qsettings.h"))
 
 
 
