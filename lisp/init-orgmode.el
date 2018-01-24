eda04960ccea3ca33075d49b5911916fff3ddb2b
masterwrw <master.wrw@gmail.com>
2018-01-24 12:33:07 +0800

    disable org-bullets if not on linux, disable agenda files

--- lisp/init-orgmode.el
+++ lisp/init-orgmode.el
@@ -33,17 +33,19 @@
 (setq org-src-fontify-natively t)
 
 
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda ()
-			     (org-bullets-mode 1))))
+(unless *is-linux*
+  (use-package org-bullets
+    :ensure t
+    :config
+    (add-hook 'org-mode-hook (lambda ()
+                               (org-bullets-mode 1))))
+  )
 
 
 ;; Index for all org files
-(load-library "find-lisp")
-(if (file-exists-p "~/notebook/notes")
-  (setq org-agenda-files (find-lisp-find-files "~/notebook/notes" "\.org$")))
+;(load-library "find-lisp")
+;(if (file-exists-p "~/notebook/notes")
+;  (setq org-agenda-files (find-lisp-find-files "~/notebook/notes" "\.org$")))
 
 
 
