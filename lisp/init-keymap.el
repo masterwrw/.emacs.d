c9fa7cf83d252f1bbfe319196c1ea2d720aad24d
masterwrw <master.wrw@gmail.com>
2018-01-25 16:01:34 +0800

    add key bindings for isearch

--- lisp/init-keymap.el
+++ lisp/init-keymap.el
@@ -129,6 +129,19 @@
 (bind-key "M-<backspace>" 'backward-kill-word)
 
 
+;;; isearch configuration
+(progn
+  ;; set arrow keys in isearch. left/right is backward/forward, up/down is history. press Return to exit
+  (define-key isearch-mode-map (kbd "<up>") 'isearch-ring-retreat )
+  (define-key isearch-mode-map (kbd "<down>") 'isearch-ring-advance )
+
+  (define-key isearch-mode-map (kbd "<left>") 'isearch-repeat-backward)
+  (define-key isearch-mode-map (kbd "<right>") 'isearch-repeat-forward)
+
+  (define-key minibuffer-local-isearch-map (kbd "<left>") 'isearch-reverse-exit-minibuffer)
+  (define-key minibuffer-local-isearch-map (kbd "<right>") 'isearch-forward-exit-minibuffer))
+
+
 (defhydra hydra-code-browser (:color blue)
   "
 _a_ dumb go             _v_ xref reference      _e_ hide block
