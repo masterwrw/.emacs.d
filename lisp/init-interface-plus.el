c043eb1a3fa0793cf66a565f54bc8392b93a5ff8
masterwrw <master.wrw@gmail.com>
2018-01-25 14:20:47 +0800

    add search function, add cursor type and mode config

--- lisp/init-interface-plus.el
+++ lisp/init-interface-plus.el
@@ -1,5 +1,12 @@
 ;;; Interface plus
 
+;; Cursor shape
+(setq-default cursor-type 'bar)
+
+;; Make cursor not blink
+(blink-cursor-mode 0)
+
+
 ;; diminish, Diminished modes are minor modes with no modeline display
 (require-package 'diminish)
 (require 'diminish)
@@ -60,8 +67,8 @@
 ;; elscreen, Emacs window session manager, like tab bar.
 (require-package 'elscreen)
 (require 'elscreen)
-(elscreen-start)
-(require 'setup-elscreen)
+;(elscreen-start)
+;(require 'setup-elscreen)
 
 
 ;; beacon, Highlight the cursor whenever the window scrolls
@@ -157,6 +164,32 @@
   (require 'server)
   (if (not (server-running-p))
       (server-start)))
+
+
+;; Copy from prelude config: https://github.com/bbatsov/prelude/blob/master/core/prelude-core.el
+(defun prelude-search (query-url prompt)
+  "Open the search url constructed with the QUERY-URL.
+PROMPT sets the `read-string prompt."
+  (browse-url
+   (concat query-url
+           (url-hexify-string
+            (if mark-active
+                (buffer-substring (region-beginning) (region-end))
+              (read-string prompt))))))
+
+(defmacro prelude-install-search-engine (search-engine-name search-engine-url search-engine-prompt)
+  "Given some information regarding a search engine, install the interactive command to search through them"
+  `(defun ,(intern (format "prelude-%s" search-engine-name)) ()
+       ,(format "Search %s with a query or region if any." search-engine-name)
+       (interactive)
+       (prelude-search ,search-engine-url ,search-engine-prompt)))
+
+(prelude-install-search-engine "google"     "http://www.google.com/search?q="              "Google: ")
+(prelude-install-search-engine "youtube"    "http://www.youtube.com/results?search_query=" "Search YouTube: ")
+(prelude-install-search-engine "github"     "https://github.com/search?q="                 "Search GitHub: ")
+(prelude-install-search-engine "duckduckgo" "https://duckduckgo.com/?t=lm&q="              "Search DuckDuckGo: ")
+(prelude-install-search-engine "bing"       "https://www.bing.com/search?q="               "Bing: ")
+
 
 
 ;; persp-mode, windows/buffers sets shared among frames + save/load.
