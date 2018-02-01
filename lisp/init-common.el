f5f957d8431eb133ecab8b1fb122a58f93220fed
masterwrw <master.wrw@gmail.com>
2018-02-01 19:01:50 +0800

    add auto comment line function

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -115,6 +115,26 @@ Version 2017-12-04"
             (forward-char)))))))
 
 
+(defun xah-comment-dwim ()
+  "Like `comment-dwim', but toggle comment if cursor is not at end of line.
+
+URL `http://ergoemacs.org/emacs/emacs_toggle_comment_by_line.html'
+Version 2016-10-25"
+  (interactive)
+  (if (region-active-p)
+      (comment-dwim nil)
+    (let (($lbp (line-beginning-position))
+          ($lep (line-end-position)))
+      (if (eq $lbp $lep)
+          (progn
+            (comment-dwim nil))
+        (if (eq (point) $lep)
+            (progn
+              (comment-dwim nil))
+          (progn
+            (comment-or-uncomment-region $lbp $lep)
+            (forward-line )))))))
+
 
 
 (provide 'init-common)
