94aa3ecd24a52da8bfb5ac8297462c22801d7df6
soeye <master.wrw@gmail.com>
2018-05-01 01:20:47 +0800

    move package init and garbage config to config.org

--- init.el
+++ init.el
@@ -1,51 +1,7 @@
-;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
+(package-initialize)
 
-(defvar startup/file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
+(require 'org)
 
-(defun startup/revert-file-name-handler-alist ()
-  (setq file-name-handler-alist startup/file-name-handler-alist))
-
-(defun startup/reset-gc ()
-  (setq gc-cons-threshold 16777216
-	gc-cons-percentage 0.1))
-
-(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
-(add-hook 'emacs-startup-hook 'startup/reset-gc)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; This is all kinds of necessary
-(require 'package)
-(setq package-enable-at-startup nil) ;; not activat installed packages
-
-(setq package-archives
-      '(("gnu" . "http://elpa.emacs-china.org/gnu/")
-	("melpa" . "http://elpa.emacs-china.org/melpa/")
-	;;("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")
-	;;("marmalade" . "http://marmalade-repo.org/packages/")
-	;;("org" . "https://orgmode.org/elpa/")
-	))
-(package-initialize) ;; activate installed packages
-
-;; On-demand installation of packages
-(defun require-package (package &optional min-version no-refresh)
-  "Ask elpa to install given PACKAGE."
-  (if (package-installed-p package min-version)
-      t
-    (if (or (assoc package package-archive-contents) no-refresh)
-        (package-install package)
-      (progn
-        (package-refresh-contents)
-        (require-package package min-version t)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; Bootstrapping use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;;; This is the actual config file.
-(when (file-readable-p "~/.emacs.d/config.org")
-  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(setq config-file (expand-file-name "config.org" "~/.emacs.d/"))
+(when (file-readable-p config-file)
+  (org-babel-load-file config-file))
