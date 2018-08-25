cbcc91c08b5d201ac0444a384779b3ea6d724de7
soeye <master.wrw@gmail.com>
2018-08-25 12:22:27 +0800

    change mouse wheel scroll

--- config/init-edit.el
+++ config/init-edit.el
@@ -14,7 +14,14 @@
 (use-package ivy
   :ensure t)
 
-(setq scroll-conservatively 100)
+;;; 按行滚动
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+(setq scroll-conservatively 10000)
+(setq auto-window-vscroll nil)
 
 (use-package which-key
   :ensure t
