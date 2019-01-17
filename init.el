ae6e209a0b184a5c173ad6426e3792412a148198
soeye <owenlang@163.com>
2019-01-18 01:25:42 +0800

    add imenu regexp on emacs-lisp-mode

--- init.el
+++ init.el
@@ -1217,7 +1217,12 @@
                ))
   (add-hook hook '(lambda () (qt-mode-style-setup))))
 
-;;; Elisp
+;;;;; Elisp
+(defun imenu-elisp-sections ()
+  (setq imenu-prev-index-position-function nil)
+  (add-to-list 'imenu-generic-expression '("Sections" "^;;;; \\(.+\\)$" 1) t))
+
+(add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'yas-minor-mode)
 (add-hook 'emacs-lisp-mode-hook
