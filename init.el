3feb7c770e29ef9d9ef6c91adbc0f28a0b3ad7c3
soeye <owenlang@163.com>
2019-01-19 16:45:57 +0800

    remove flycheck and flycheck-posframe

--- init.el
+++ init.el
@@ -1402,23 +1402,6 @@
                 (ibuffer-switch-to-saved-filter-groups "EL")))
 (setq ibuffer-show-empty-filter-groups nil)
 
-;;;; flycheck
-;; 不使用 global-flycheck-mode
-(require 'flycheck)
-(dolist (hook (list
-               'ruby-mode-hook
-               'python-mode-hook
-               'go-mode-hook
-               'c++-mode-hook
-               ))
-  (add-hook hook '(lambda () (flycheck-mode 1))))
-
-;; Use posframe as flycheck UI.
-(if (>= emacs-major-version 26)
-    (with-eval-after-load 'flycheck
-      (require 'flycheck-posframe)
-      (add-hook 'flycheck-mode-hook #'flycheck-posframe-mode)))
-
 ;;;; yasnippet
 (require 'yasnippet)
 ;; (require 'yasnippet-snippets) ;; can not found in load-path?? 
