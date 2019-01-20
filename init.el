3acc3287a441816f446b8af09ed6b4c0f4bd103d
soeye <owenlang@163.com>
2019-01-20 21:52:48 +0800

    remove paredit

--- init.el
+++ init.el
@@ -607,16 +607,6 @@
   (dired-hide-details-mode 1))
 (add-hook 'dired-mode-hook 'eye-dired-mode-setup)
 
-(require 'paredit)
-;;(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-;;(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-;;(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-;;(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-;;(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-;;(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-;;(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
-
-
 (require 'wgrep)
 (require 'wgrep-ag)
 
