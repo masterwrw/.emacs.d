de7e6e244a42093705e5475aa29feeb27875a6b8
soeye <owensys@hotmail.com>
2019-04-05 18:42:59 +0800

    change open bash auto close shell command output window

--- lisp/init-external.el
+++ lisp/init-external.el
@@ -1,8 +1,10 @@
 
 (defun eye/open-bash ()
   (interactive)
-  (async-shell-command "C:\\software\\PortableGit\\git-bash.exe"))
-
+  (async-shell-command "C:\\software\\PortableGit\\git-bash.exe")  
+  ;;if use async-shell-command, can't kill that buffer
+  ;; (kill-buffer "*Shell Command Output*")
+  (delete-other-windows))
 
 
 
