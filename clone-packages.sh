d5b96fa299ebab2553388d11212230ff82a909ac
soeye <owensys@hotmail.com>
2019-04-06 00:33:30 +0800

    add clone-packages.sh

---
+++ clone-packages.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+mkdir ~/packages
+cd ~/packages
+
+git clone https://github.com/nschum/idle-require.el.git idle-require
+git clone https://github.com/immerrr/lua-mode.git
+git clone https://github.com/joaotavora/yasnippet.git
+git clone https://gitlab.com/vancan1ty/emacs-backward-forward.git
+git clone https://github.com/magnars/dash.el.git dash
+git clone https://github.com/emacs-helm/helm.git
+git clone https://github.com/areina/helm-dash.git
+git clone https://github.com/Wilfred/helpful.git
+git clone https://github.com/magit/magit.git
+git clone https://github.com/emacs-php/php-mode.git
+git clone https://github.com/xuchunyang/youdao-dictionary.el.git youdao-dictionary
+git clone https://github.com/EricCrosson/qt-pro-mode.git
+git clone https://github.com/cataska/qml-mode.git
+git clone https://github.com/cute-jumper/company-qml.git
+git clone https://github.com/redguardtoo/counsel-etags.git
+git clone https://github.com/jacktasia/dumb-jump.git
+git clone https://github.com/paradoxxxzero/jinja2-mode.git
+git clone https://github.com/emacsmirror/rainbow-mode.git
+git clone https://github.com/nflath/hungry-delete.git
+git clone https://github.com/mhayashi1120/Emacs-wgrep.git
+git clone https://github.com/justbur/emacs-which-key.git
+git clone https://github.com/abo-abo/avy.git
+git clone https://github.com/magnars/multiple-cursors.el.git multiple-cursors
+git clone https://github.com/enoson/eno.el.git eno
+git clone https://github.com/joostkremers/writeroom-mode.git
+git clone https://github.com/jwiegley/emacs-async.git
+git clone https://github.com/abo-abo/swiper.git
+git clone https://github.com/nonsequitur/smex.git
+git clone https://github.com/technomancy/find-file-in-project.git
+git clone https://github.com/company-mode/company-mode.git
+git clone https://github.com/randomphrase/company-c-headers.git
+git clone https://github.com/zenitani/elisp.git
+git clone https://github.com/pitkali/pos-tip.git
+git clone https://github.com/xuchunyang/chinese-word-at-point.el.git chinese-word-at-point
+git clone https://github.com/auto-complete/popup-el.git popup
+git clone https://github.com/Malabarba/names.git
+git clone https://github.com/hniksic/emacs-htmlize.git
+git clone https://github.com/magnars/s.el.git s
+git clone https://github.com/rejeep/f.el.git f
+git clone https://github.com/enoson/edit-at-point.el.git edit-at-point
+git clone https://github.com/joostkremers/visual-fill-column.git
+git clone https://github.com/magit/ghub.git
+git clone https://github.com/vermiculus/graphql.el.git graphql
+git clone https://github.com/volrath/treepy.el.git treepy
+git clone https://github.com/magit/with-editor.git
+git clone https://github.com/magit/magit-popup.git
+git clone https://github.com/xcwen/ac-php.git
+git clone https://github.com/bbatsov/projectile.git
+git clone https://github.com/manateelazycat/color-rg.git
+git clone https://github.com/tumashu/company-posframe.git
+git clone https://github.com/tuhdo/semantic-refactor.git
+git clone https://github.com/tumashu/posframe.git
+git clone https://github.com/Wilfred/elisp-refs.git
+git clone https://github.com/Wilfred/loop.el.git loop
+git clone https://github.com/cask/shut-up.git
+git clone https://github.com/kuanyui/moe-theme.el.git moe-theme
+git clone https://github.com/manateelazycat/aweshell.git
+git clone https://github.com/bbatsov/super-save.git
+git clone https://github.com/fxbois/web-mode.git
+git clone https://github.com/Kungsgeten/yankpad.git
+git clone https://github.com/zargener/emacs-password-genarator.git
+git clone https://github.com/manateelazycat/watch-other-window.git
+git clone https://github.com/tumashu/ivy-posframe.git
+git clone https://github.com/dajva/rg.el.git rg rg
+git clone https://github.com/bbatsov/solarized-emacs.git
+git clone https://github.com/jordonbiondo/ample-theme.git
