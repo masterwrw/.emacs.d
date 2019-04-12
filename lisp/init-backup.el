b0389e28e6650da718be33c0f317ef20732c37e7
soeye <owensys@hotmail.com>
2019-04-12 12:11:05 +0800

    enable global-auto-revert-mode; improve create tmp and bak directory

--- lisp/init-backup.el
+++ lisp/init-backup.el
@@ -1,11 +1,11 @@
 ;;;; Backup
 (defvar user-cache-directory "~/tmp/emacs_cache")
-(file-exists-p user-cache-directory)
-(unless (file-exists-p "~/tmp")
+
+(unless (file-directory-p "~/tmp")
   (make-directory "~/tmp"))
-(unless (file-exists-p user-cache-directory)
+(unless (file-directory-p user-cache-directory)
   (make-directory user-cache-directory))
-(unless (file-exists-p (concat user-cache-directory "/bak"))
+(unless (file-directory-p (concat user-cache-directory "/bak"))
   (make-directory (concat user-cache-directory "/bak")))
 ;; 备份文件 file~，指定备份目录后，文件名为 !drive_f!dirname!dirname!filename~
 (setq backup-by-copying t)
@@ -17,6 +17,10 @@
 ;; 临时文件 #file#
 (setq auto-save-default t) ;; 开启自动备份临时文件，auto-save.el 中会修改这个变量
 (setq auto-save-file-name-transforms '((".*" "~/tmp/emacs_cache/bak" t))) ;; 设置备份文件目录
+(setq auto-save-timeout 5)
+
+(global-auto-revert-mode 1)
+
 
 
 
