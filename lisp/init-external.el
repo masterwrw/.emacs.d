6bceb02a95338eb022ff2d5962f073236560f611
soeye <owensys@hotmail.com>
2019-04-13 06:29:21 +0800

    fix open-external no check executable exists; add some program

--- lisp/init-external.el
+++ lisp/init-external.el
@@ -12,7 +12,11 @@
 (setq eye-program-alist '(("bash" . "C:/software/PortableGit/git-bash.exe")
 			  ("keepassx" . "C:/software/KeePassX-2.0.3/KeePassX.exe")
 			  ("qtcreator" . "c:/Qt/Qt5.7.1/Tools/QtCreator/bin/qtcreator.exe")
-			  ("file manager" . (lambda () (eye/open-file-manager)))))
+			  ("file manager" . (lambda () (eye/open-file-manager)))
+			  ("xfce terminal" . "xfce4-terminal")
+			  ("calc" . "c:/Windows/System32/calc.exe")
+			  ("github desktop" . "C:/Users/soeye/AppData/Local/GitHubDesktop/GitHubDesktop.exe")
+			  ))
 (defun eye/open-external ()
   "Open locale program"
   (interactive)
@@ -24,7 +28,10 @@
     (when sel
       (let ((value (cdr (assoc sel eye-program-alist))))
 	(if (stringp value)
-	    (async-shell-command value)
+	    (progn
+	      (if (executable-find value)
+		  (async-shell-command value)
+		(message "Can not find program: %s" value)))
 	  (funcall value))))
     (delete-other-windows)))
 
