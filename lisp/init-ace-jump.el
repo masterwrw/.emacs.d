23ece0265624b70876aa6ce750064d4284454bc1
owensys <owensys@hotmail.com>
2019-04-22 13:07:28 +0800

    disable hydra-jump

--- lisp/init-ace-jump.el
+++ lisp/init-ace-jump.el
@@ -1,8 +1,8 @@
 (require-maybe 'ace-jump-mode)
-(defhydra+ hydra-jump (:exit t :idle 1.0)
-  ("c" ace-jump-char-mode "Jump char")
-  ("l" ace-jump-line-mode "Jump line")
-  )
+;; (defhydra+ hydra-jump (:exit t :idle 1.0)
+  ;; ("c" ace-jump-char-mode "Jump char")
+  ;; ("l" ace-jump-line-mode "Jump line")
+  ;; )
 
 
 
