91b7e2ee8905614ab99ae30a64da8371ad1d4c43
owensys <owensys@hotmail.com>
2019-05-08 10:20:23 +0800

    change dired config, hide . and ..

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -5,20 +5,22 @@
   "Show/hide dot-files"
   (interactive)
   (when (equal major-mode 'dired-mode)
-        (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
-                (progn 
-                  (set (make-local-variable 'dired-dotfiles-show-p) nil)
-                  (message "h")
-                  (dired-mark-files-regexp "^\\\.")
-                  (dired-do-kill-lines))
-          (progn (revert-buffer) ; otherwise just revert to re-show
-                         (set (make-local-variable 'dired-dotfiles-show-p) t)))))
+    (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
+        (progn 
+          (set (make-local-variable 'dired-dotfiles-show-p) nil)
+          (message "h")
+          (dired-mark-files-regexp "^\\\.")
+          (dired-do-kill-lines))
+      (progn (revert-buffer) ; otherwise just revert to re-show
+             (set (make-local-variable 'dired-dotfiles-show-p) t)))))
 
 
 ;; 隐藏 dired 中文件拥有者和文件权限等信息
 (defun eye-dired-mode-setup ()
   "hide the file's unix owner and permission info"
-  (dired-hide-details-mode 1))
+  (dired-hide-details-mode 1)		;隐藏以.开头的文件
+  (dired-omit-mode 1)			;隐藏.和..本身 @see https://stackoverflow.com/questions/43628315/how-to-hide-one-dot-current-directory-in-dired-mode
+  )
 (add-hook 'dired-mode-hook 'eye-dired-mode-setup)
 
 
