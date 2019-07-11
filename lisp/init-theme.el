b84cda90144830a4eda05942adbbbf8dd5b7efef
owensys <owensys@hotmail.com>
2019-07-11 11:43:05 +0800

    Change default theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,15 +1,20 @@
 ;; (load-theme 'wombat t)
 
-(require 'solarized)
+;; (require 'solarized)
 ;; make the modeline high contrast
-(setq solarized-high-contrast-mode-line t)
+;; (setq solarized-high-contrast-mode-line t)
 ;; Use less bolding
-(setq solarized-use-less-bold t)
+;; (setq solarized-use-less-bold t)
 ;; Use more italics
-(setq solarized-use-more-italic t)
-(load-theme 'solarized-dark t)
+;; (setq solarized-use-more-italic t)
+;; (load-theme 'solarized-dark t)
 
-(set-face-attribute 'which-func nil :foreground "#003300")
+
+(require 'naysayer-theme)
+(load-theme 'naysayer t)
+
+
+;; (set-face-attribute 'which-func nil :foreground "#003300")
 
 ;; set some faces color
 ;; (set-face-attribute 'default nil :foreground "#daa520")
